71f3cac96a8a5472fd5ab3607072e922e0f61034
DavidTurkGT <dlturk747@gmail.com>
2017-07-07 16:58:40 -0400

    Added PUT /api/todos/:id endpoint

--- routes/todos.js
+++ routes/todos.js
@@ -2,6 +2,17 @@ const express   = require('express');
 const router    = express.Router();
 const models    = require('../models');
 ////////////////////////////////////////////////////////////////////////////////
+//Middleware
+function validateBody (req, res, next) {
+  req.checkBody('title', 'Title cannot be empty').notEmpty();
+  req.checkBody('order', 'Order cannot be empty').notEmpty();
+  req.checkBody('completed', 'Completed cannot be empty').notEmpty();
+  let errors = req.validationErrors();
+  if(errors) res.status(400).send("Bad request. Try again");
+
+  next();
+}
+////////////////////////////////////////////////////////////////////////////////
 router.get('/', async (req, res) => {
   //return a JSON array of todo items
   todos = await models.todo.findAll()
@@ -15,13 +26,8 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) =>{
+router.post('/', validateBody, async (req, res) =>{
   //post a JSON representation of a todo and have it saved. Returns the saved todo item in JSON.
-  //Body Validation
-  req.checkBody('title', 'Title cannot be empty').notEmpty();
-  let errors = req.validationErrors();
-  if(errors) res.status(400).send("Bad request. Try again");
-  //Create new Todo
   let newTodo = {
     title: req.body.title,
     order: Number(req.body.order),
@@ -47,9 +53,23 @@ router.get('/:id', async (req, res) =>{
   }
 });
 
-router.put('/:id', (req, res) =>{
+router.put('/:id', validateBody, async (req, res) =>{
   //update a todo item. Returns the modified todo item.
-  res.send("This will update a specific item (using ID parameter)");
+  let todo = await models.todo.update({
+    title: req.body.title,
+    order: req.body.order,
+    completed: req.body.completed
+  },
+  {
+    where: { id: req.params.id}
+  })
+    .catch( (err) => res.status(500).send("Internal server error"));
+  console.log("Todo: ",todo);
+  if(todo[0]){
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(todo);
+  }
+  else res.status(404).send("Error: todo not found");
 });
 
 router.patch('/:id', (req, res) =>{
