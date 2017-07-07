6a298db579e61b7e52ce58be37fd8ae3637246f7
DavidTurkGT <dlturk747@gmail.com>
2017-07-07 14:55:07 -0400

    Initial commit

---
+++ migrations/20170707184727-create-todo.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      order: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('todos');
+  }
+};
